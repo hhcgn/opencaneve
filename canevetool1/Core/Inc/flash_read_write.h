f4150d551e430637e6e4b9766965e3c9f40e0931
hhcgn <40318619+hhcgn@users.noreply.github.com>
2022-07-16 19:58:49 +0800

    Delete canevetool1 directory

--- canevetool1/Core/Inc/flash_read_write.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef  _FLASH_READ_WRITE_H
-#define  _FLASH_READ_WRITE_H
-#include "stm32f1xx_hal.h"
-
-extern uint8_t flash_data_read[10];
-int flashWriteData(int paramSizeKb, uint64_t data[], size_t len);
-void flashReadData(int paramSizeKb, uint8_t *data, size_t len);
-#endif
--- canevetool1/Core/Inc/stringchange.h
+++
@@ -1,9 +0,0 @@
-#ifndef __STRINGCHANGE_H
-#define __STRINGCHANGE_H 			   
-//#include "stm32f10x.h"
-char* itoa(int num,char* str,int radix);
-int ctoi(char *str);
-int detopwm(int *deg);
-long hexToDec(char *source);
-unsigned long long  numtoc(int n);
-#endif
